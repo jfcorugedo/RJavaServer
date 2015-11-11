6a4c8c9528f11bb0a5eb016d8c01edce2293141a
jfcorugedo <jfcorugedo@gmail.com>
2015-11-11 16:30:55 +0100

    Remove unused imports

--- src/test/java/com/jfcorugedo/rserver/engine/RServeEngineProviderServiceIT.java
+++ src/test/java/com/jfcorugedo/rserver/engine/RServeEngineProviderServiceIT.java
@@ -24,7 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
