a53c92a2c7f9ba0956546710d314bdb772aedf2a
jfcorugedo <jfcorugedo@gmail.com>
2016-03-09 14:54:15 +0100

    removes unused imports

--- src/main/java/com/jfcorugedo/rserver/Application.java
+++ src/main/java/com/jfcorugedo/rserver/Application.java
@@ -1,6 +1,5 @@
 package com.jfcorugedo.rserver;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
